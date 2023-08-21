d9b8a6ccfbf966ae61986269895c20c1db931b07
machangzhi <18791373741@163.com>
2023-08-22 00:27:09 +0800

    路由基本使用

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-// 引用koa
+// 引用各类依赖
 const Koa = require("koa")
+const Router = require("koa-router")
 // 实例化koa
 const app = new Koa()
+// 实例化路由
+const router = new Router()
 // 定义端口
 const port = 3000
 
-app.use((ctx) => {
-  ctx.body = "hello world"
+router.get("/", (ctx) => {
+  ctx.body = "主页"
 })
+
+router.get("/users", (ctx) => {
+  ctx.body = "用户列表"
+})
+
+router.post("/users", (ctx) => {
+  ctx.body = "创建用户"
+})
+
+router.get("/users/:id", (ctx) => {
+  ctx.body = "用户" + ctx.params.id
+})
+
+app.use(router.routes())
 
 app.listen(port)
