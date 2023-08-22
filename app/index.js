27ebeb4c0498d9cbebe5fb58b14a6187defb0dac
machangzhi <18791373741@163.com>
2023-08-22 23:58:58 +0800

    安装cross-env，配置启动命令以及koa-json-error生产和开发环境报错信息展示

--- app/index.js
+++ app/index.js
@@ -8,19 +8,24 @@ const app = new Koa()
 const routing = require("./routes")
 // 定义端口
 const port = 3000
+// 自定义错误无处理
+// app.use(async (ctx, next) => {
+//   try {
+//     await next()
+//   } catch (error) {
+//     ctx.status = error.status || error.statusCode || 500
+//     ctx.body = {
+//       message: error.message,
+//     }
+//   }
+// })
 
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (error) {
-    ctx.status = error.status || error.statusCode || 500
-    ctx.body = {
-      message: error.message,
-    }
-  }
-})
-
-app.use(error())
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) =>
+      process.env.NODE_ENV === "production" ? rest : { stack, ...rest },
+  })
+)
 app.use(bodyparser())
 // 自动化注册路由
 routing(app)
