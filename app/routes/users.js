db8ddc30b2b5868a42bc192b9ceaaa514829ad9f
machangzhi <18791373741@163.com>
2023-08-22 23:09:21 +0800

    重构user控制器

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,31 +2,23 @@
 const Router = require("koa-router")
 // 实例化路由
 const router = new Router({ prefix: "/users" })
+// 引入控制器
+const {
+  find,
+  findById,
+  create,
+  update,
+  delete: del,
+} = require("../controllers/users")
 
-// 定义内存数据库
-const db = [{ name: "李雷" }]
+router.get("/", find)
 
-router.get("/", (ctx) => {
-  ctx.body = db
-})
+router.post("/", create)
 
-router.post("/", (ctx) => {
-  db.push(ctx.request.body)
-  ctx.body = ctx.request.body
-})
+router.get("/:id", findById)
 
-router.get("/:id", (ctx) => {
-  ctx.body = db[ctx.params.id * 1]
-})
+router.put("/:id", update)
 
-router.put("/:id", (ctx) => {
-  db[ctx.params.id * 1] = ctx.request.body
-  ctx.body = ctx.request.body
-})
-
-router.delete("/:id", (ctx) => {
-  db.splice(ctx.params.id * 1, 1)
-  ctx.status = 204
-})
+router.delete("/:id", del)
 
 module.exports = router
