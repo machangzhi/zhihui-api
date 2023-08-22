cdd4efa5e4260d37d5f5d7a741d3d651bf27d34f
machangzhi <18791373741@163.com>
2023-08-22 23:47:00 +0800

    安装koa-json-error，并使用koa-json-error

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 // 引用各类依赖
 const Koa = require("koa")
 const bodyparser = require("koa-bodyparser")
+const error = require("koa-json-error")
 // 实例化koa
 const app = new Koa()
 // 引入自动注册路由函数
@@ -19,10 +20,11 @@ app.use(async (ctx, next) => {
   }
 })
 
+app.use(error())
 app.use(bodyparser())
 // 自动化注册路由
 routing(app)
 
 app.listen(port, () => {
-  console.log(`应启动在${port}端口上`)
+  console.log(`应用启动在${port}端口上`)
 })
